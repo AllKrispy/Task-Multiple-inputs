440b14c21c639af84fc4871570ca4904099f8761
AllKrispy <70009510+AllKrispy@users.noreply.github.com>
2020-08-25 19:48:31 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int num1,num2,num3;
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+    printf("Enter third number: ");
+    scanf("%d",&num3);
+    printf("Sum of %d,%d and %d is %d",num1,num2,num3,num1+num2+num3);
+    return 0;
+}
